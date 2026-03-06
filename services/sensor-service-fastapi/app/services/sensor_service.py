f9b1d190b445a65e1011868d87bf09fcee3c1bf6
Sasindu Denuwan <sasindudenuwan2006wpsk@gmail.com>
2026-03-06 12:45:08 +0530

    Implameneted the "Automation Service" for the AGMS

---
+++ services/sensor-service-fastapi/app/services/sensor_service.py
@@ -0,0 +1,17 @@
+# app/services/sensor_service.py
+from datetime import datetime
+
+sensor_history = []
+
+def save_sensor_data(data):
+    record = {
+        "zoneId": data.zoneId,
+        "temperature": data.temperature,
+        "humidity": data.humidity,
+        "timestamp": datetime.now()
+    }
+    sensor_history.append(record)
+    return record
+
+def get_all_sensor_data():
+    return sensor_history
